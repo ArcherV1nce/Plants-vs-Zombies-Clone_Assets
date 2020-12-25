3a1d3c65ea12b83be2df4078f36d837a15fb3fda
ArcherV1nce <ArcherV1nce@users.noreply.github.com>
2020-12-25 21:38:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Plants-vs-Zombies-Clone_Assets
+ Assets for Plants vs Zombies studying project. No build settings included.
---
+++ Scripts/LoadLeverController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLeverController : MonoBehaviour
+{
+    [SerializeField] private string levelName;
+
+    public void LoadLevel()
+    {
+
+        if(levelName != null)
+        {
+            SceneManager.LoadSceneAsync(levelName);
+        }
+
+    }
+
+}
