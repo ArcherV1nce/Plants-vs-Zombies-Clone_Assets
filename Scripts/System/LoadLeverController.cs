973d2afab0425184f5b695fe4f06e1eb17379bd1
ArcherV1nce <glowbradyon@gmail.com>
2021-01-11 22:50:15 +0200

    Loading scene init. Work in progress.
     Work in progress.

---
+++ Scripts/System/LoadLeverController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLeverController : MonoBehaviour
+{
+    [SerializeField] private string levelName = "SplashScreen";
+    [SerializeField] private int timeToWait = 5;
+    [SerializeField] private bool sceneLoadingFinished = false;
+
+
+    /*
+    * Загрузка должна проиходить в той сцене, в которой находится объект с текстом.
+     * До этого должна загрузиться загрузочная сцена. 
+     * Обновить курутину загрузкой сцены с текстом, после которой осуществляется загрузка следующей сцены.
+     */
+
+    public void LoadLevel()
+    {
+
+        if(levelName != null)
+        {
+            SceneManager.LoadSceneAsync(levelName);
+        }
+
+    }
+
+    public void LoadWithDelay()
+    {
+        if (levelName != null)
+        {
+            StartCoroutine(DelayLoadLevel());
+        }
+    }
+
+    private IEnumerator DelayLoadLevel()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        SceneManager.LoadSceneAsync(levelName);
+        yield return 0;
+    }
+
+    public void LoadScreenLoadLevel()
+    {
+        StartCoroutine(LoadScreenOperationProcess());    
+    }
+
+    private IEnumerator LoadScreenOperationProcess()
+    {
+        AsyncOperation sceneLoadingOperation = SceneManager.LoadSceneAsync(levelName);
+        DontDestroyOnLoad(this.gameObject);
+        sceneLoadingFinished = sceneLoadingOperation.isDone;
+        while (!sceneLoadingFinished)
+        {
+            sceneLoadingFinished = sceneLoadingOperation.isDone;
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForEndOfFrame();
+        Destroy(this.gameObject);
+        StopCoroutine(LoadScreenOperationProcess());
+    }
+
+    public bool GetLoadLevelStatus()
+    {
+        return sceneLoadingFinished;
+    }
+
+
+
+}
---
+++ Scripts/System/LoadingText.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoadingText : MonoBehaviour
+{
+    [Header("Parameters")]
+    [SerializeField] TextMeshProUGUI loadingTextObject = null;
+    [SerializeField] string loadingText = "";
+    [SerializeField] float textUpdateTime = 0.3f;
+    [SerializeField] private protected int dotsMaxAmount = 3;
+
+    
+    [Header("Sceme Status")]
+    [SerializeField] bool isLoaded = false;
+    [SerializeField] LoadLeverController leverController = null;
+
+    private void Start()
+    {
+        StartCoroutine(LoadingTextUpdate());
+        leverController = FindObjectOfType<LoadLeverController>();
+    }
+
+    private void Update()
+    {
+        isLoaded = leverController.GetLoadLevelStatus();
+    }
+
+    private IEnumerator LoadingTextUpdate()
+    {
+        int i = 0;
+        string outputText = loadingText;
+        loadingTextObject.text = outputText;
+        while (!isLoaded)
+        {
+            if (i < dotsMaxAmount)
+            {
+                i++;
+                outputText = outputText + ".";
+            }
+            else if (i >= 3)
+            {
+                i = 0;
+                outputText = loadingText;
+            }
+
+            loadingTextObject.text = outputText;
+
+            yield return new WaitForSeconds(textUpdateTime);
+        }
+
+        StopCoroutine(LoadingTextUpdate());
+    }
+
+    /*
+     * Загрузка должна проиходить в той сцене, в которой находится объект с текстом.
+     * До этого должна загрузиться загрузочная сцена. 
+     * Обновить курутину загрузкой сцены с текстом, после которой осуществляется загрузка следующей сцены.
+     */
+
+
+}
